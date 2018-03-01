ac8b8b7396b8ff462559beb0d1626f3b784d6527
unknown <raul.arturo798@gmail.com>
2018-03-01 17:27:35 -0400

    test font message

---
+++ src/BoilerPlate/App.cpp
@@ -0,0 +1,419 @@
+#include "App.hpp"
+#include <iostream>
+#include <algorithm>
+
+// OpenGL/SDL includes
+//#include <irrklang/irrKlang.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_ttf.h>
+
+namespace Engine
+{
+	App::App(const std::string& title, const int width, const int height)
+		: m_title(title)
+		, m_width(width)
+		, m_height(height)
+		, m_nUpdates(0)
+		, m_timer(new TimeManager)
+		, m_mainWindow(nullptr)
+	{
+		m_state = GameState::UNINITIALIZED;
+		m_lastFrameTime = m_timer->GetElapsedTimeInSeconds();
+
+		game = new Game();
+	}
+
+	//dsssssssssssss
+	TTF_Font *m_font;
+
+
+	App::~App()
+	{
+		game->RemoveFromMemory();
+
+		CleanupSDL();
+	}
+
+	void App::Execute()
+	{
+		if (m_state != GameState::INIT_SUCCESSFUL)
+		{
+			std::cerr << "Game INIT was not successful." << std::endl;
+			return;
+		}
+
+		m_state = GameState::RUNNING;
+
+		SDL_Event event;
+		while (m_state == GameState::RUNNING)
+		{
+			// Input polling
+			while (SDL_PollEvent(&event))
+			{
+				OnEvent(&event);
+			}
+
+			Update();
+			Render();
+		}
+	}
+
+	bool App::Init()
+	{
+		if (TTF_Init() == -1) {
+			SDL_Log("TTF_Init: %s\n", TTF_GetError());
+			return false;
+		}
+
+		m_font = TTF_OpenFont("Font.ttf", 100);
+
+		SDL_version compile_version;
+		const SDL_version *link_version = TTF_Linked_Version();
+		SDL_TTF_VERSION(&compile_version);
+
+		SDL_Log("compiled with SDL_ttf version: %d.%d.%d\n",
+			compile_version.major,
+			compile_version.minor,
+			compile_version.patch);
+
+		SDL_Log("running with SDL_ttf version: %d.%d.%d\n",
+			link_version->major,
+			link_version->minor,
+			link_version->patch);
+
+
+		// Init the external dependencies
+		bool success = SDLInit() && GlewInit();
+		if (!success)
+		{
+			m_state = GameState::INIT_FAILED;
+			return false;
+		}
+
+		// Setup the viewport
+		SetupViewport();
+
+		// Change game state
+		m_state = GameState::INIT_SUCCESSFUL;
+
+		return true;
+	}
+
+	void App::OnKeyDown(SDL_KeyboardEvent keyBoardEvent)
+	{
+		switch (keyBoardEvent.keysym.scancode)
+		{
+		case SDL_SCANCODE_W:
+			SDL_Log("Up");
+			game->inputManager.SetW(true);
+			break;
+
+		case SDL_SCANCODE_A:
+			SDL_Log("Left");
+			game->inputManager.SetA(true);
+			break;
+
+		case SDL_SCANCODE_D:
+			SDL_Log("Right");
+			game->inputManager.SetD(true);
+			break;
+
+		case SDL_SCANCODE_R:
+			SDL_Log("Remove Asteroid");
+			game->inputManager.SetR(true);
+			break;
+
+		case SDL_SCANCODE_T:
+			SDL_Log("Add Asteroid");
+			game->inputManager.SetT(true);
+			break;
+
+		case SDL_SCANCODE_F:
+			SDL_Log("frame Rate");
+			game->inputManager.SetF(true);
+			break;
+
+		case SDL_SCANCODE_G:
+			SDL_Log("Debugger Mode");
+			game->inputManager.SetG(true);
+			break;
+
+		case SDL_SCANCODE_Y:
+			SDL_Log("Reset Player");
+			game->inputManager.SetY(true);
+			break;
+
+		case SDL_SCANCODE_SPACE:
+			SDL_Log("Shoot");
+			game->inputManager.SetSpace(true);
+			break;
+
+		}
+	}
+
+	void App::OnKeyUp(SDL_KeyboardEvent keyBoardEvent)
+	{
+		switch (keyBoardEvent.keysym.scancode)
+		{
+		case SDL_SCANCODE_W:
+			game->inputManager.SetW(false);
+			break;
+
+		case SDL_SCANCODE_A:
+			game->inputManager.SetA(false);
+			break;
+
+		case SDL_SCANCODE_D:
+			game->inputManager.SetD(false);
+			break;
+
+		case SDL_SCANCODE_R:
+			game->inputManager.SetR(false);
+			break;
+
+		case SDL_SCANCODE_T:
+			game->inputManager.SetT(false);
+			break;
+
+		case SDL_SCANCODE_F:
+			game->inputManager.SetF(false);
+			break;
+
+		case SDL_SCANCODE_G:
+			game->inputManager.SetG(false);
+			break;
+
+		case SDL_SCANCODE_Y:
+			game->inputManager.SetY(false);
+			break;
+
+		case SDL_SCANCODE_SPACE:
+			game->inputManager.SetSpace(false);
+			break;
+
+		default:
+			SDL_Log("%S was pressed.", keyBoardEvent.keysym.scancode);
+			break;
+		}
+	}
+
+	void App::Update()
+	{
+		double startTime = m_timer->GetElapsedTimeInSeconds();
+		double calculateEndTime = m_timer->GetElapsedTimeInSeconds();
+
+		//update Game (asteroids, player and bullts)
+		game->Update(m_width, m_height, game->CalculateFrameRate(calculateEndTime, startTime));
+
+		double endTime = m_timer->GetElapsedTimeInSeconds();
+		double nextTimeFrame = startTime + DESIRED_FRAME_TIME;
+	
+		//update game frame rate 
+		game->UpdateFrameRate(endTime, startTime);
+
+		while (endTime < nextTimeFrame)
+		{
+			// Spin lock
+			endTime = m_timer->GetElapsedTimeInSeconds();
+		}
+
+		//double elapsedTime = endTime - startTime;        
+		m_lastFrameTime = m_timer->GetElapsedTimeInSeconds();
+
+		m_nUpdates++;
+	}
+
+	unsigned int power_two_floor(unsigned int val) {
+		unsigned int power = 2, nextVal = power * 2;
+		while ((nextVal *= 2) <= val)
+			power *= 2;
+		return power * 2;
+	}
+
+	void RenderText(std::string message, SDL_Color color, float x, float y, int size)
+	{
+		glLoadIdentity();
+		glTranslatef(x, y, 0.f);
+
+		SDL_Surface *surface;
+
+		//Render font to a SDL_Surface
+		if ((surface = TTF_RenderText_Blended(m_font, message.c_str(), color)) == nullptr) {
+			TTF_CloseFont(m_font);
+			std::cout << "TTF_RenderText error: " << std::endl;
+			return;
+		}
+
+		GLuint texId;
+
+		//Generate OpenGL texture
+		glEnable(GL_TEXTURE_2D);
+		glGenTextures(1, &texId);
+		glBindTexture(GL_TEXTURE_2D, texId);
+
+		//Avoid mipmap filtering
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+		//Find the first power of two for OpenGL image 
+		int w = power_two_floor(surface->w) * 2;
+		int h = power_two_floor(surface->h) * 2;
+
+		//Create a surface to the correct size in RGB format, and copy the old image
+		SDL_Surface * s = SDL_CreateRGBSurface(0, w, h, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
+
+		SDL_BlitSurface(surface, NULL, s, NULL);
+
+		//Copy the created image into OpenGL format
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, w, h, 0, GL_BGRA, GL_UNSIGNED_BYTE, s->pixels);
+
+		//Draw the OpenGL texture as a Quad
+		glBegin(GL_QUADS); {
+			glTexCoord2d(0, 1); glVertex3f(0, 0, 0);
+			glTexCoord2d(1, 1); glVertex3f(0 + surface->w, 0, 0);
+			glTexCoord2d(1, 0); glVertex3f(0 + surface->w, 0 + surface->h, 0);
+			glTexCoord2d(0, 0); glVertex3f(0, 0 + surface->h, 0);
+		} glEnd();
+		glDisable(GL_TEXTURE_2D);
+
+		//Cleanup
+		SDL_FreeSurface(s);
+		SDL_FreeSurface(surface);
+		glDeleteTextures(1, &texId);
+	}
+
+	void App::Render()
+	{
+		ColorPalette color;
+		glClearColor(color.getDarkSlateGrayRedValue(), color.getDarkSlateGrayGreenValue(), color.getDarkSlateGrayBlueValue(), color.getDarkSlateGrayAlphaValue());
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		game->Render();
+
+		SDL_Color green;
+		green.r = 0;
+		green.g = 255;
+		green.b = 0;
+		green.a = 0;
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		RenderText("Test Message", green, -100.0f, -25.0f, 50);
+
+
+		SDL_GL_SwapWindow(m_mainWindow);
+	}
+
+	bool App::SDLInit()
+	{
+		// Initialize SDL's Video subsystem
+		if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+		{
+			std::cerr << "Failed to init SDL" << std::endl;
+			return false;
+		}
+
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+		Uint32 flags = SDL_WINDOW_OPENGL |
+			SDL_WINDOW_SHOWN |
+			SDL_WINDOW_RESIZABLE;
+
+		m_mainWindow = SDL_CreateWindow(
+			m_title.c_str(),
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			m_width,
+			m_height,
+			flags
+		);
+
+		if (!m_mainWindow)
+		{
+			std::cerr << "Failed to create window!" << std::endl;
+			SDL_Quit();
+			return false;
+		}
+
+		m_context = SDL_GL_CreateContext(m_mainWindow);
+		SDL_GL_MakeCurrent(m_mainWindow, m_context);
+
+		// Make double buffer interval synced with vertical scanline refresh
+		SDL_GL_SetSwapInterval(0);
+
+		return true;
+	}
+
+	void App::SetupViewport()
+	{
+		// Defining ortho values
+		float halfWidth = m_width * 0.5f;
+		float halfHeight = m_height * 0.5f;
+
+		// Set viewport to match window
+		glViewport(0, 0, m_width, m_height);
+
+		// Set Mode to GL_PROJECTION
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+
+		// Set projection MATRIX to ORTHO
+		glOrtho(-halfWidth, halfWidth, -halfHeight, halfHeight, -1, 1);
+
+		// Setting Mode to GL_MODELVIEW
+		glMatrixMode(GL_MODELVIEW);
+	}
+
+	bool App::GlewInit()
+	{
+		GLenum err = glewInit();
+		if (GLEW_OK != err)
+		{
+			std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+			return false;
+		}
+
+		return true;
+	}
+
+	void App::CleanupSDL()
+	{
+		// Cleanup
+		SDL_GL_DeleteContext(m_context);
+		SDL_DestroyWindow(m_mainWindow);
+
+		SDL_Quit();
+	}
+
+	void App::OnResize(int width, int height)
+	{
+		// TODO: Add resize functionality
+
+		m_width = width;
+		m_height = height;
+
+		SetupViewport();
+	}
+
+	void App::OnExit()
+	{
+		// Exit main for loop
+		m_state = GameState::QUIT;
+
+		// Cleanup SDL pointers
+		CleanupSDL();
+	}
+
+	
+
+
+	
+}
+
+
